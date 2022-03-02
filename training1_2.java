3409a820d298f38e1cf940ac94934728137b8ebf
doishimpeiu <gazadaba38@yahoo.co.jp>
2022-03-03 01:58:50 +0900

    2nd

---
+++ training1_2.java
@@ -0,0 +1,11 @@
+public class training1_2 {
+  public static void main(String[] args) {
+    int a;
+    int b;
+    int c;
+    a = 3;
+    b = 5;
+    c = a * b;
+    System.out.println("長方形の面積は" + c + "です。");
+  }
+}

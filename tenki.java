46b48c7e2d66cd25b55b6acbad3a54bf751752b8
doishimpeiu <gazadaba38@yahoo.co.jp>
2022-03-03 02:36:20 +0900

    3章の参照

---
+++ tenki.java
@@ -0,0 +1,10 @@
+public class tenki {
+  public static void main(String[] args) {
+    boolean tenki = true;
+    if (tenki == true) {
+      System.out.println("洗濯をします。");
+    } else {
+      System.out.println("DVDを観ます");
+    }
+  }
+}
---
+++ toire.java
@@ -0,0 +1,8 @@
+public class toire {
+  public static void main(String[] args) {
+    boolean doorClose = true;
+    while (doorClose == true) {
+      System.out.println("ノックする");
+    }
+  }
+}
